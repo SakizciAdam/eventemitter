6fa32f7ca76d4e872f9691b2639c4ac32b7336e7
SakizciAdam <sakizciadam2000@gmail.com>
2022-02-27 18:56:15 +0300

    Added async support

--- eventemitter.go
+++ eventemitter.go
@@ -3,7 +3,11 @@ package eventemitter
 type EventBus struct {
 	Listeners map[string][]func(...interface{})
 
-	Emit func(name string, args ...interface{})
+	emit func(name string, async bool, args ...interface{})
+
+	EmitSync func(name string, args ...interface{})
+
+	EmitAsync func(name string, args ...interface{})
 
 	AddListener func(name string, listener func(...interface{}))
 }
@@ -13,14 +17,26 @@ func New() *EventBus {
 
 	bus.Listeners = make(map[string][]func(...interface{}))
 
-	bus.Emit = func(name string, args ...interface{}) {
+	bus.emit = func(name string, async bool, args ...interface{}) {
 		for key, val := range bus.Listeners {
 			if key == name {
 				for _, listener := range val {
-					listener(args...)
+					if async {
+						go listener(args...)
+					} else {
+						listener(args...)
+					}
 				}
 			}
 		}
+	}
+
+	bus.EmitSync = func(name string, args ...interface{}) {
+		bus.emit(name, false, args...)
+	}
+
+	bus.EmitAsync = func(name string, args ...interface{}) {
+		bus.emit(name, true, args...)
 	}
 
 	bus.AddListener = func(name string, listener func(...interface{})) {
